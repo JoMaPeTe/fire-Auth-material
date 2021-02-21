4482c39dce78c2145185957fb77e60d833a0b47d
JoseMPeTe <jmperezterrero@gmail.com>
2021-02-21 21:30:55 +0100

    Escribimos función para obtener userid a partir de su email y hacerlo admin

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    //obtener user y añadirle custom claim (convertirlo en admin)
+    //para obtener el valor se hace return de toda la promise
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        //ya podemos sacar el user.uid correspondiente a ese email
+        //con el uid le añadimos el custom claim
+        return admin.auth().setCustomUserClaims(user.uid, {
+            //aquí un objeto con la claim
+            admin: true
+        });
+    }).then(() => {
+        return {
+            message: `Succes! ${data.email} has been made an admin`
+        }
+    }).catch(err => {
+        return err;
+    });
+});
